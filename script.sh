77aa09e68d59cad40ebe1d3a08a87704ca5e5f94
mwarko <marcantoinefournier@bettersec.fr>
2015-08-10 18:04:15 +0200

    sed qui marche

--- script.sh
+++ script.sh
@@ -4,3 +4,5 @@
 i=1
 
 for ligne in `ls Diaporama`;do echo "slides[$i] = new Array(15, \"$i\", \"Diaporama/$ligne\");">>diapo.txt;i=$(($i+1));done
+cat diapo.txt | tr -d '\r\n' > diapo.txt
+sed -i -e "s|remplacericipls|`cat diapo.txt`|" index.html
