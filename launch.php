1687d5eb77f7ecf31785c0f930641edaf55337bf
mwarko <marcantoinefournier@bettersec.fr>
2015-08-11 12:05:21 +0200

    okok

---
+++ launch.php
@@ -0,0 +1,3 @@
+<?
+exec("/var/www/script.sh");
+?>
--- script.sh
+++ script.sh
@@ -4,5 +4,10 @@
 i=1
 
 for ligne in `ls Diaporama`;do echo "slides[$i] = new Array(15, \"$i\", \"Diaporama/$ligne\");">>diapo.txt;i=$(($i+1));done
-cat diapo.txt | tr -d '\r\n' > diapo.txt
-sed -i -e "s|remplacericipls|`cat diapo.txt`|" index.html
+cat diapo.txt | tr -d '\r\n' > diapo2.txt
+sed -i -e "s|remplacericipls|`cat diapo2.txt`|" index.html
+
+rm -f diapo.txt
+rm -f diapo2.txt
+
+service apache2 restart
